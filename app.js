e0b9506784c0e2a220a95580534c3be4fdae6ccc
dimast90 <dimastr90@gmail.com>
2020-06-26 14:42:52 +0300

    Reformat and key fixes

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const config = require('config');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
-app.use(express.json({extended: true}));
+app.use(express.json({ extended: true }));
 
+app.use("/api/cat", require("./routes/cat.routes"));
+app.use("/api/items", require("./routes/items.routes"));
+app.use("/api/bases", require("./routes/bases.routes"));
 
-app.use('/api/cat', require('./routes/cat.routes'));
-app.use('/api/items', require('./routes/items.routes'));
-app.use('/api/bases', require('./routes/bases.routes'));
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
-
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
-const PORT = config.get('port') || 5000;
+const PORT = config.get("port") || 5000;
 
 async function start() {
-    try{
-        await mongoose.connect(config.get('mongoUri'), {
+    try {
+        await mongoose.connect(config.get("mongoUri"), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
-        })
+            useCreateIndex: true,
+        });
     } catch (e) {
-        console.log('Server Error', e.message);
+        console.log("Server Error", e.message);
         process.exit(1);
     }
 }
 
 start();
 
-app.listen(PORT, ()=> console.log(`Server is start on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is start on port: ${PORT}`));
--- client/src/components/Bases/BaseMain/BaseMain.js
+++ client/src/components/Bases/BaseMain/BaseMain.js
@@ -1,29 +1,42 @@
 import React from "react";
-import classes from "./BaseMain.module.css"
+import classes from "./BaseMain.module.css";
 import BaseMainTable from "./BaseMainTable/BaseMainTable";
 
-
 const BaseMain = (props) => {
-
     return (
         <>
             <div className={classes.addItemBlock}>
                 <div className={classes.addItemToBaseCat}>
                     <h6>Category:</h6>
-                    <select className="browser-default custom-select" onChange={props.onSelectCatHandler}>
-                        {props.allNotEmptyCategories.length > 0 && props.allNotEmptyCategories.map(i => {
-                            return <option key={'cat-opt-key-' + i._id}>{i._id}</option>
-                        })}
+                    <select
+                        className="browser-default custom-select"
+                        onChange={props.onSelectCatHandler}
+                    >
+                        {props.allNotEmptyCategories.length > 0 &&
+                            props.allNotEmptyCategories.map((i) => {
+                                return (
+                                    <option key={"cat-opt-key-" + i._id}>
+                                        {i._id}
+                                    </option>
+                                );
+                            })}
                     </select>
                 </div>
 
                 <div className={classes.addItemToBaseName}>
                     <h6>Item:</h6>
-                    <select className="browser-default custom-select" onChange={props.onSelectItemHandler}>
-                        {props.allNotEmptyCategories.length > 0 && props.currentItemListForAdd.map(i => {
-                            return <option key={'item-opt-key-' + i}>{i}</option>
-                        })
-                        }
+                    <select
+                        className="browser-default custom-select"
+                        onChange={props.onSelectItemHandler}
+                    >
+                        {props.allNotEmptyCategories.length > 0 &&
+                            props.currentItemListForAdd.map((i) => {
+                                return (
+                                    <option key={"item-opt-key-" + i}>
+                                        {i}
+                                    </option>
+                                );
+                            })}
                     </select>
                 </div>
 
@@ -31,46 +44,72 @@ const BaseMain = (props) => {
                     <h6>Quantity:</h6>
                     <div className="input-group mb-3">
                         <div className="input-group-prepend">
-                            <span className="input-group-text" id="inputGroup-sizing-default">Qty</span>
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Qty
+                            </span>
                         </div>
-                        <input type="text" className="form-control" aria-label="Default"
-                               aria-describedby="inputGroup-sizing-default" value={props.currentQtyForAdd}
-                               onChange={props.onQtyChangeHandler}/>
+                        <input
+                            type="text"
+                            className="form-control"
+                            aria-label="Default"
+                            aria-describedby="inputGroup-sizing-default"
+                            value={props.currentQtyForAdd}
+                            onChange={props.onQtyChangeHandler}
+                        />
                     </div>
                 </div>
 
                 <div className={classes.addItemToBaseButton}>
-                    <button className="btn btn-success" onClick={props.addItemButtonHandler}>Add item</button>
+                    <button
+                        className="btn btn-success"
+                        onClick={props.addItemButtonHandler}
+                    >
+                        Add item
+                    </button>
                 </div>
-
             </div>
-
 
             <table className="table table-striped">
                 <thead>
-                <tr>
-                    {props.tableHeadItems.map(i=>{
-                        return <th><button className={classes.basesTableHeadButtons} value={i.value} onClick={props.sortButtonsHandler}>
-                            {i.name}<i className="fa fa-sort" aria-hidden="true"/></button></th>
-                    })}
-                    <th>Edit</th>
-                </tr>
+                    <tr>
+                        {props.tableHeadItems.map((i) => {
+                            return (
+                                <th key={"baseMain-key-" + i.value}>
+                                    <button
+                                        className={
+                                            classes.basesTableHeadButtons
+                                        }
+                                        value={i.value}
+                                        onClick={props.sortButtonsHandler}
+                                    >
+                                        {i.name}
+                                        <i
+                                            className="fa fa-sort"
+                                            aria-hidden="true"
+                                        />
+                                    </button>
+                                </th>
+                            );
+                        })}
+                        <th>Edit</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {props.currentSelectedBaseData.map(i => (
-                    <BaseMainTable
-                        key={"TableBaseDatakey-" + i.uniq_Id}
-                        data={i}
-                        removeBaseItemHandler={props.removeBaseItemHandler}
-                        renameBaseItemHandler={props.renameBaseItemHandler}
-                    />
-                ))}
-
+                    {props.currentSelectedBaseData.map((i) => (
+                        <BaseMainTable
+                            key={"TableBaseDatakey-" + i.uniq_Id}
+                            data={i}
+                            removeBaseItemHandler={props.removeBaseItemHandler}
+                            renameBaseItemHandler={props.renameBaseItemHandler}
+                        />
+                    ))}
                 </tbody>
             </table>
         </>
-    )
+    );
 };
-
 
 export default BaseMain;
